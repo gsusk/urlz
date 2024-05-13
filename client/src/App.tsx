58e7049650ba0cf350c0b6abcec5a8b785af7aeb
gsus <satehortuamercado@gmail.com>
2024-05-13 18:40:42 -0500

    add initial layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <body></body>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
