8b01e5717a751ffe6a296fa8936d9327a8244bb0
gsus <satehortuamercado@gmail.com>
2024-05-15 04:00:46 -0500

    added home presentation

---
+++ client/src/components/Shortener.tsx
@@ -0,0 +1,5 @@
+function Shortener() {
+  return <div>Shortener</div>;
+}
+
+export default Shortener;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,28 @@
+import Shortener from "../components/Shortener";
+
 function Home() {
   return (
     <div>
-      <section>This is the Home</section>
+      <section className="main-container">
+        <div className="presentation">
+          <h1 className="title">URLZY</h1>
+          <small>
+            <i>Shorten Any Link</i>
+          </small>
+          <div className="main-description">
+            <p>
+              Introducing , the ultimate URL shortening tool designed to
+              simplify your links and streamline your online experience. With
+              [Your Project Name], long and cumbersome URLs become concise and
+              shareable with just a click
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="main-container">
+        <Shortener />
+        <div>asd</div>
+      </section>
     </div>
   );
 }
