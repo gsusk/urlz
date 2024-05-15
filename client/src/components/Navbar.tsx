13f054a7cd63d5d21ca063d2254a484d2478de3c
gsus <satehortuamercado@gmail.com>
2024-05-15 02:47:42 -0500

    settled initial button css

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 function Navbar() {
   return (
     <div className="nav-container">
-      <div className="logo-cont">
+      <div className="img-c-fit">
         <img src="/logosm.png" alt="logosmall" />
       </div>
       <div className="grow-end-c">
-        <button className="button __vmc">Sign Up</button>
-        <button className="button __vmc">Sign In</button>
+        <button className="button __vsc">Sign Up</button>
+        <button className="button __vsc">Sign In</button>
       </div>
     </div>
   );
