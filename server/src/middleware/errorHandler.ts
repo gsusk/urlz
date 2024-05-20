a5e535f6e37ad21ff9f8ca47547ba66a4f6d305c
gsus <satehortuamercado@gmail.com>
2024-05-20 16:38:16 -0500

    custom errors

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,10 +1,61 @@
 import { NextFunction, Request, Response } from 'express';
 import { Prisma } from '@prisma/client';
 
+export enum HttpStatus {
+  CONTINUE = 100,
+  SWITCHING_PROTOCOLS = 101,
+  PROCESSING = 102,
+  EARLYHINTS = 103,
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  NON_AUTHORITATIVE_INFORMATION = 203,
+  NO_CONTENT = 204,
+  RESET_CONTENT = 205,
+  PARTIAL_CONTENT = 206,
+  AMBIGUOUS = 300,
+  MOVED_PERMANENTLY = 301,
+  FOUND = 302,
+  SEE_OTHER = 303,
+  NOT_MODIFIED = 304,
+  TEMPORARY_REDIRECT = 307,
+  PERMANENT_REDIRECT = 308,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  PAYMENT_REQUIRED = 402,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  METHOD_NOT_ALLOWED = 405,
+  NOT_ACCEPTABLE = 406,
+  PROXY_AUTHENTICATION_REQUIRED = 407,
+  REQUEST_TIMEOUT = 408,
+  CONFLICT = 409,
+  GONE = 410,
+  LENGTH_REQUIRED = 411,
+  PRECONDITION_FAILED = 412,
+  PAYLOAD_TOO_LARGE = 413,
+  URI_TOO_LONG = 414,
+  UNSUPPORTED_MEDIA_TYPE = 415,
+  REQUESTED_RANGE_NOT_SATISFIABLE = 416,
+  EXPECTATION_FAILED = 417,
+  I_AM_A_TEAPOT = 418,
+  MISDIRECTED = 421,
+  UNPROCESSABLE_ENTITY = 422,
+  FAILED_DEPENDENCY = 424,
+  PRECONDITION_REQUIRED = 428,
+  TOO_MANY_REQUESTS = 429,
+  INTERNAL_SERVER_ERROR = 500,
+  NOT_IMPLEMENTED = 501,
+  BAD_GATEWAY = 502,
+  SERVICE_UNAVAILABLE = 503,
+  GATEWAY_TIMEOUT = 504,
+  HTTP_VERSION_NOT_SUPPORTED = 505,
+}
+
 export class ValidationError extends Error {
   constructor(
     public message: string,
-    public statusCode: number,
+    public statusCode: HttpStatus,
     public errors: string[] | string,
   ) {
     super(message);
@@ -25,7 +76,7 @@ export class ValidationError extends Error {
 export class AuthorizationError extends Error {
   constructor(
     public message: string,
-    public statusCode: number,
+    public statusCode: HttpStatus,
     public errors: string[] | string,
   ) {
     super(message);
@@ -46,7 +97,7 @@ export class AuthorizationError extends Error {
 export class CustomError extends Error {
   constructor(
     public message: string,
-    public statusCode: number,
+    public statusCode: HttpStatus,
   ) {
     super(message);
     this.name = 'CustomError';
@@ -73,7 +124,6 @@ export function errorHandler(
   if (process.env.NODE_ENV === 'production') {
     let error = err;
 
-    error.message = err.message;
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
       console.log('handlePrismaError');
       error = handlePrismaError(err);
@@ -81,48 +131,56 @@ export function errorHandler(
       error = handleJWTError();
     } else if (error.name === 'TokenExpiredError') {
       error = handleJWTError();
+    } else {
+      error = new CustomError('Something went wrong', HttpStatus.BAD_REQUEST);
     }
     return sendErrorProd(error, request, response);
   }
 }
 
-function sendErrorDev(err: Error, req: any, res: any) {
-  throw new Error('Function not implemented.');
+function sendErrorDev(err: Error, _req: Request, res: Response) {
+  let status;
+  if (
+    err instanceof CustomError ||
+    err instanceof AuthorizationError ||
+    err instanceof ValidationError
+  ) {
+    status = err.statusCode;
+    console.log(err.serializeError());
+    res.status(status).json(err);
+  } else {
+    res.status(400).json(err);
+  }
 }
 
-function sendErrorProd(err: Error, req: any, res: any) {
-  throw new Error('Function not implemented.');
+function sendErrorProd(err: Error, _req: Request, res: Response) {
+  if (
+    err instanceof CustomError ||
+    err instanceof AuthorizationError ||
+    err instanceof ValidationError
+  ) {
+    const status = err.statusCode;
+    console.log(err.serializeError());
+    res.status(status).json(err.serializeError());
+  } else {
+    res
+      .status(500)
+      .json({ name: 'Unexpected Error', message: 'Something went wrong' });
+  }
 }
 
-function handlePrismaError(err: Prisma.PrismaClientKnownRequestError): {
-  name: string;
-  message: string;
-  stack?: string | undefined;
-} {
+function handlePrismaError(err: Prisma.PrismaClientKnownRequestError) {
   switch (err.code) {
     case 'P2002':
-      console.log(err.meta.target);
-      return {
-        name: 'P2002',
-        message: 'Invalid input syntax for integer: "1"',
-      };
-    case 'P2004':
-      return {
-        name: 'P2004',
-        message: 'Invalid input syntax for integer: "1"',
-      };
-    case 'P2002':
-      // handling duplicate key errors
-      return new CustomError(`Duplicate field value: ${err.meta.target}`, 400);
-    case 'P2014':
-      // handling invalid id errors
-      return new CustomError(`Invalid ID: ${err.meta.target}`, 400);
+      return new CustomError('Duplicate field value', 400);
     case 'P2003':
-      // handling invalid data errors
-      return new CustomError(`Invalid input data: ${err.meta.target}`, 400);
+      return new CustomError('Invalid input data', 400);
+    case 'P2006':
+      return new CustomError('Invalid input type', 400);
+    case 'P2014':
+      return new CustomError(`Invalid Change`, 400);
     default:
-      // handling all other errors
-      return new CustomError(`Something went wrong: ${err.message}`, 500);
+      return new CustomError(`Something went wrong`, 500);
   }
 }
 
@@ -131,5 +189,5 @@ function handleJWTError(): {
   message: string;
   stack?: string | undefined;
 } {
-  throw new Error('Function not implemented.');
+  return new CustomError('', 200);
 }
