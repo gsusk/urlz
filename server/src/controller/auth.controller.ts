4f5c295c601f11312499e436cb8175985ebadf01
gsus <satehortuamercado@gmail.com>
2024-05-21 13:01:03 -0500

    add password hashing an signIn

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -2,15 +2,41 @@ import { prisma } from '../db';
 import { SignInSchema, SignUpSchema } from '../validations/schemas';
 import { Request, Response, NextFunction } from 'express';
 import bcrypt from 'bcrypt';
+import { CustomError } from '../utils/customErrors';
+import { HttpStatus } from '../constants/httpStatus';
 
-export const signIn = (
+export const signIn = async (
   request: Request<unknown, unknown, SignInSchema>,
   response: Response,
   next: NextFunction,
 ) => {
-  request;
-  response;
-  next;
+  try {
+    const { username, password } = request.body;
+    const user = await prisma.user.findUnique({
+      select: {
+        password: true,
+        username: true,
+        email: true,
+      },
+      where: {
+        username: username,
+      },
+    });
+    if (!user) {
+      return next(
+        new CustomError('Invalid User or Password', HttpStatus.NOT_FOUND),
+      );
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(
+        new CustomError('Invalid User or Password', HttpStatus.NOT_FOUND),
+      );
+    }
+    response.status(200).json({ username: user.username, email: user.email });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const signUp = async (
@@ -20,8 +46,8 @@ export const signUp = async (
 ) => {
   try {
     const { username, email, password } = request.body;
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     const user = await prisma.user.create({
       data: {
         username: username,
