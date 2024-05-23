e48c9a0dca562529330c93bad27cf7cf5431ecc5
gsus <satehortuamercado@gmail.com>
2024-05-23 01:25:13 -0500

    login form added

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 function LoginForm() {
-  return <div>Login THIS IS THE LOGIIIIN</div>;
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    const id = e.currentTarget.id;
+    setFormData((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
