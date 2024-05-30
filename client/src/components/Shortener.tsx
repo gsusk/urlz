eb5caa596c58dfbc4eaa124f136f0d99c51b9f1f
gsuskre <satehortuamercado@gmail.com>
2024-05-30 16:51:59 -0500

    added shortener details

--- client/src/components/Shortener.tsx
+++ client/src/components/Shortener.tsx
@@ -1,11 +1,24 @@
 import { FaLink } from "react-icons/fa";
 import { LuBrush } from "react-icons/lu";
 import "./shortener.css";
+import { useState } from "react";
 
 function Shortener() {
+  const [form, setform] = useState({ longUrl: "", customUrl: "" });
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const id = e.currentTarget.id;
+    const value = e.currentTarget.value;
+    setform((prev) => {
+      return {
+        ...prev,
+        [id]: value,
+      };
+    });
+  };
+
   return (
     <section>
-      <div className="shortener-container">
+      <form className="shortener-container">
         <label htmlFor="longUrl">
           <FaLink />
           <span>Shorten a long URL</span>
@@ -15,6 +28,9 @@ function Shortener() {
           name="longUrl"
           id="longUrl"
           className="long-url-input"
+          value={form.longUrl}
+          onChange={handleChange}
+          placeholder="Enter long url"
         />
         <label htmlFor="customUrl">
           <LuBrush />
@@ -25,10 +41,13 @@ function Shortener() {
           name="customUrl"
           id="customUrl"
           className="custom-url-inut"
+          value={form.customUrl}
+          onChange={handleChange}
+          placeholder="Enter Custom Alias"
         />
 
-        <input type="submit" />
-      </div>
+        <button type="submit">Shorten</button>
+      </form>
     </section>
   );
 }
