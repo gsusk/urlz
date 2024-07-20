2cd299c70e292c04b64165c32aa5797f82712022
gsus <satehortuamercado@gmail.com>
2024-07-19 23:08:14 -0500

    change to MyIMage component

--- client/src/components/MyImage.tsx
+++ client/src/components/MyImage.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 type Props = {
   src: string;
   alt: string;
-  width: string;
-  height: string;
+  width?: string;
+  height?: string;
 };
 
 function MyImage({ src, alt, width, height }: Props) {
@@ -12,25 +12,32 @@ function MyImage({ src, alt, width, height }: Props) {
 
   const handleLoad = () => setLoading(false);
 
+  const config = useMemo(() => {
+    return {
+      width: width ?? "inherit",
+      height: height ?? "inherit",
+      borderRadius: "inherit",
+    };
+  }, [width, height]);
+
   return (
     <>
       <img
         src={src}
         alt={alt}
-        width={width}
-        height={height}
         style={{
           display: loading ? "none" : "block",
-          width,
-          height,
-          borderRadius: "inherit",
+          ...config,
         }}
         onLoad={handleLoad}
         fetchPriority="low"
       />
       <div
         className="loading-image"
-        style={{ width, height, display: loading ? "block" : "none" }}
+        style={{
+          ...config,
+          display: loading ? "block" : "inline",
+        }}
       ></div>
     </>
   );
