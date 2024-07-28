78634d9a9f713f5907ebb4d358dfc179ecbc17e7
gsus <satehortuamercado@gmail.com>
2024-07-27 21:22:27 -0500

    added update to controller

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,8 @@
 import { UrlSchema } from '../validations/schemas';
-import { getUserProfile } from '../controller/user.controller';
+import {
+  getUserProfile,
+  updateUserProfile,
+} from '../controller/user.controller';
 import { authMiddleware } from '../middlewares/token';
 import { validation } from '../middlewares/validationMiddleware';
 import { Router } from 'express';
@@ -7,5 +10,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/profile', authMiddleware, getUserProfile);
+router.put('/profile', authMiddleware, updateUserProfile);
 
 export default router;
