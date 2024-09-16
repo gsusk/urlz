63bcccbf72333363ea1ba20578b78bd96e446d09
gsus <satehortuamercado@gmail.com>
2024-09-16 05:05:24 -0500

    fixed geolocation

--- server/src/routes/short.ts
+++ server/src/routes/short.ts
@@ -2,22 +2,15 @@ import { validation } from '../middlewares/validationMiddleware';
 import { createCustomUrl, shortenUrl } from '../controller/url.controller';
 import { Router } from 'express';
 import { CustomUrlSchema, UrlSchema } from '../validations/schemas';
-import { geolocation } from '../utils/ip';
 import { guestOrUser } from '../middlewares/token';
 
 const router = Router();
 
-router.post(
-  '/create',
-  validation(UrlSchema),
-  geolocation,
-  guestOrUser,
-  shortenUrl,
-);
+router.post('/create', validation(UrlSchema), guestOrUser, shortenUrl);
+
 router.post(
   '/custom',
   validation(CustomUrlSchema),
-  geolocation,
   guestOrUser,
   createCustomUrl,
 );
