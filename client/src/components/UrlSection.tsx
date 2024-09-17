07faf873956be929e0a95566d9e476c79821a5cd
gsus <satehortuamercado@gmail.com>
2024-09-17 16:37:40 -0500

    added func to detect click outside

--- client/src/components/UrlSection.tsx
+++ client/src/components/UrlSection.tsx
@@ -3,6 +3,8 @@ import { IoEarth } from "react-icons/io5";
 import { IoSettingsSharp } from "react-icons/io5";
 import { FaEye } from "react-icons/fa";
 import "./Url.css";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 export type PropTypes = {
   views: number;
@@ -17,7 +19,32 @@ export default function Url({
   shortenedUrl,
   originalUrl,
 }: PropTypes) {
+  const [isOpen, setIsOpen] = useState(false);
+  const settingRef = useRef<HTMLDivElement>(null);
   const composedUrl = `http://localhost:8081/${shortenedUrl}`;
+
+  const toggleMenu = (e: React.FormEvent) => {
+    e.stopPropagation();
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    console.log("sssss");
+    const handleCLickOutside = (event: MouseEvent) => {
+      console.log("fff");
+      if (
+        settingRef.current &&
+        !settingRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handleCLickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleCLickOutside, true);
+    };
+  }, []);
+
   return (
     <div className="urli-container">
       <div className="url-fp">
@@ -51,13 +78,21 @@ export default function Url({
         <div className="tdt">
           <div>{creationDate}</div>
         </div>
-        <div className="tend">
+        <div className="tend" onClick={toggleMenu} ref={settingRef}>
           <IoSettingsSharp />
-          <div className="tend-dropdown-menu">
-            <a>Copy</a>
-            <a>Stats</a>
-            <a>Delete</a>
-          </div>
+          {isOpen && (
+            <div className="tend-dropdown-menu">
+              <NavLink className="navlink-sett" to={"/"}>
+                Copy
+              </NavLink>
+              <NavLink className="navlink-sett" to={"/"}>
+                Stats
+              </NavLink>
+              <NavLink className="navlink-sett" to={"/"}>
+                Delete
+              </NavLink>
+            </div>
+          )}
         </div>
       </div>
     </div>
