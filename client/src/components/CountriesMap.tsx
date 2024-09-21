fe115f13fa7e854fa87fbe1ba193ed2e6b8cf864
gsus <satehortuamercado@gmail.com>
2024-09-21 03:50:35 -0500

    added polygon series to add border to map

--- client/src/components/CountriesMap.tsx
+++ client/src/components/CountriesMap.tsx
@@ -23,30 +23,10 @@ function CountriesMap() {
         geoJSON: am5geodata_worldLow,
       })
     );
-
-    polygonSeries.data.push({
-      geometry: {
-        type: "Polygon",
-        coordinates: [
-          [
-            [26.5936, 55.6676],
-            [26.175, 55.0033],
-            [25.8594, 54.9192],
-            [25.5473, 54.3317],
-            [24.7683, 53.9746],
-            [23.4845, 53.9398],
-            [23.37, 54.2005],
-            [22.7663, 54.3568],
-            [22.8311, 54.8384],
-            [21.2358, 55.2641],
-            [21.0462, 56.07],
-            [22.0845, 56.4067],
-            [24.1206, 56.2642],
-            [24.9032, 56.3982],
-            [26.5936, 55.6676],
-          ],
-        ],
-      },
+    polygonSeries.mapPolygons.template.setAll({
+      stroke: am5.color(0xffffff),
+      strokeWidth: 1,
+      fillOpacity: 0.5,
     });
     return () => root.dispose();
   }, []);
