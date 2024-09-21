c43a69c7e878dd78d909fd308b616444146e008a
gsus <satehortuamercado@gmail.com>
2024-09-21 03:12:10 -0500

    added initial code into countries map

--- client/src/components/CountriesMap.tsx
+++ client/src/components/CountriesMap.tsx
@@ -1,9 +1,17 @@
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
 import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
+import { useLayoutEffect } from "react";
 
 function CountriesMap() {
-  return <div>CountriesMap</div>;
+  useLayoutEffect(() => {
+    let root = am5.Root.new("chartdivb");
+    let chart = root.container.children.push(
+      am5map.MapChart.new(root, { projection: am5map.geoMercator() })
+    );
+  }, []);
+
+  return <div id="chartdivb" style={{ width: "500px", height: "500px" }}></div>;
 }
 
 export default CountriesMap;
