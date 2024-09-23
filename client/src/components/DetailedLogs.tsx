67c0763186eccee121314c5dfdc15c3508c6a34a
gsus <satehortuamercado@gmail.com>
2024-09-22 23:41:47 -0500

    added details to details log component

--- client/src/components/DetailedLogs.tsx
+++ client/src/components/DetailedLogs.tsx
@@ -1,29 +1,40 @@
+import UAParser from "ua-parser-js"
+
 export type LogsPropType = {
-  details: { visitedAt: string; referrer: null | string; user_agent: string }[];
+  details: { visitedAt: string; referrer: null | string; user_agent: string, continent: string }[];
 };
 
+// const logData = useMemo(() => {
 function DetailedLogs({ details }: LogsPropType) {
+  // const parsedDetails = details.map((row) => {
+  // return (<div>sds</div>)
+  // })
+  // }, [details])
   return (
     <div>
       <div>
-        <table>
+        <table style={{ "width": "100%", "textAlign": "center" }}>
           <thead>
-            <tr>
+            <tr style={{paddingBottom: "40 px"}}>
               <th>Browser</th>
               <th>Platform</th>
               <th>Referrer</th>
               <th>Location</th>
               <th>Date</th>
+              
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>sdf</td>
-              <td>dddd</td>
-              <td>hhhh</td>
-              <td></td>
-              <td></td>
-            </tr>
+            {details.map(row => {
+              const parsedUA = new UAParser(row.user_agent)
+              return (<tr>
+                <td >{parsedUA.getBrowser().name ?? ""}</td>
+                <td >{parsedUA.getOS().name ?? ""}</td>
+                <td >{row.referrer ?? "-"}</td>
+                <td >{row.continent ?? ""}</td>
+                <td >{new Date(row.visitedAt).toLocaleString() || "-"}</td>
+              </tr>)
+            })}
           </tbody>
         </table>
       </div>
