851993a45095da6d61af5320bd51f5841b13bec3
gsus <satehortuamercado@gmail.com>
2024-10-04 08:02:42 -0500

    added redis client

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,6 +1,16 @@
 import { PrismaClient } from '@prisma/client';
+import Redis from 'ioredis';
 
 export const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
   errorFormat: 'minimal',
 });
+
+const redis = new Redis();
+
+redis.on('error', (err) => {
+  console.log('Redis Client Error', err);
+  process.exit(1);
+});
+
+export { redis };
